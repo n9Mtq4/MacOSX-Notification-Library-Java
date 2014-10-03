ab1fdc07641a1547fff0f152b33863c6741df9cc
n9Mtq4 <bill.wresnahan@gmail.com>
2014-10-03 17:21:21 -0400

    added GNU gplv3 license

--- src/com/n9mtq4/demo/notifcationmac/Notification.java
+++ src/com/n9mtq4/demo/notifcationmac/Notification.java
@@ -1,3 +1,19 @@
+/*
+ * NOTE: This is added by intellij IDE. Disregard this message if there is another copyright later in the file.
+ * Copyright (C) 2014  Will (n9Mtq4) Bresnahan
+ * A library to send notifications to the notification center in OSX
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.n9mtq4.demo.notifcationmac;
 
 import java.io.*;
